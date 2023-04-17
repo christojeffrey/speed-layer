c14b61df41709253c5cd3f9ac0e5469fa966450a
Christopher Jeffrey <christopherjeffrey492@gmail.com>
2023-04-18 00:25:16 +0700

    feat: able to connect to postgres

--- rest/index.ts
+++ rest/index.ts
@@ -6,6 +6,16 @@ app.get("/", (req, res) => {
   res.send("Well done!");
 });
 
+// get from postgres
+// pg-promise
+const pgp = require("pg-promise")();
+const db = pgp("postgres://postgres:postgres@localhost:5432/mydb");
+
+app.get("/users", async (req, res) => {
+  const users = await db.any("SELECT * FROM social_media_stats");
+  res.send(users);
+});
+
 app.listen(3000, () => {
   console.log("The application is listening on port 3000!");
 });
